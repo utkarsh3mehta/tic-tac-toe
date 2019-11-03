5b615f817e439e303a04760209841626a198de62
Utkarsh Mehta <umteappdev@mydell.com>
2019-11-04 01:16:32 +0530

    blocks logic and styles

--- src/component/Blocks/Blocks.js
+++ src/component/Blocks/Blocks.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import Block from './Block/Block';
+import styles from './Blocks.css';
 const Blocks = (props) => {
+    let rows = Object.keys(props.flag)
+    let lastrow = rows[rows.length - 1];
+    let cols = Object.keys(props.flag[lastrow])
+    let lastcol = cols[cols.length - 1]
     let blocks = Object.keys(props.flag).map(rowid => {
-        return [Object.keys(props.flag[rowid]).map(colid => {
-            return <Block key={'block'+rowid+colid} flag={props.flag[rowid][colid]}
-            row={rowid} col={colid}
-            clicked={props.blockClicked} />
-        }), <br key={'newLine' + rowid}/>]
+        let block = Object.keys(props.flag[rowid]).map(colid => {
+            if (colid === lastcol) {
+                return <Block 
+                key={'block'+rowid+colid} 
+                flag={props.flag[rowid][colid]}
+                row={rowid} col={colid}
+                lastcol
+                clicked={props.blockClicked} />
+            } else {
+                return <Block 
+                key={'block'+rowid+colid} 
+                flag={props.flag[rowid][colid]}
+                row={rowid} col={colid}
+                clicked={props.blockClicked} />
+            }
+        })
+        if(rowid === lastrow) {
+            return [block, <br key={'newline' + rowid} />]
+        }  else {
+            return [block, <hr key={'newline' + rowid} />]
+        }
     })
 
-    return blocks;
+    return (
+        <div className={styles.Blocks}>
+            {blocks}
+        </div>
+    )
 }
 export default Blocks;
