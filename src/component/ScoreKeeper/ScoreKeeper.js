0bbda178e8432d3a8d1ada114755a98956c3b93b
Utkarsh Mehta <utkarsh.mehta@fs.utc.com>
2019-11-06 20:11:25 +0530

    change color when point

--- src/component/ScoreKeeper/ScoreKeeper.js
+++ src/component/ScoreKeeper/ScoreKeeper.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import styles from './ScoreKeeper.css'
 const ScoreKeeper = (props) => {
+    let style = null
+    if(props.score === 4) {
+        style = {
+            'color': 'green'
+        }
+    }
     return (
-        <strong className={styles.ScoreKeeper}>{props.player}: {props.score}</strong>
+        <strong style={style} className={styles.ScoreKeeper}>{props.player}: {props.score}</strong>
     )
 }
 export default ScoreKeeper
