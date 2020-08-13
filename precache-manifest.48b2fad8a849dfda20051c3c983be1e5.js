fc0eaea91a05778b80e4407eeebec47fa19a9a95
Utkarsh Mehta <umtebiz@gmail.com>
2020-08-14 00:54:15 +0530

    Updates

---
+++ precache-manifest.48b2fad8a849dfda20051c3c983be1e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6907eb80adb25f6d7f4e687447b36ee9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "aac12fd5400d541c0834",
+    "url": "/tic-tac-toe/static/css/main.caad633a.chunk.css"
+  },
+  {
+    "revision": "dc8738eb8f450f0df023",
+    "url": "/tic-tac-toe/static/js/2.7b33e0d5.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/tic-tac-toe/static/js/2.7b33e0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac12fd5400d541c0834",
+    "url": "/tic-tac-toe/static/js/main.78c5fb6d.chunk.js"
+  },
+  {
+    "revision": "7e265be06da584477563",
+    "url": "/tic-tac-toe/static/js/runtime-main.d821b3e2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tic-tac-toe/static/media/logo.25bf045c.svg"
+  }
+]);
